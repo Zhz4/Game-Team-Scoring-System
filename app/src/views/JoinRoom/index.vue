763c8c40172600b9d4241baecd95d7d329d09f45
‘zhz’ <1057213071@qq.com>
2023-06-05 16:18:32 +0800

    首页样式：ok

--- app/src/views/JoinRoom/index.vue
+++ app/src/views/JoinRoom/index.vue
@@ -1,92 +1,121 @@
 <template>
-  <div>
-    <div>
-      <p>输入房间号</p>
-      <el-input v-model="roomId" placeholder="请输入房间号"/>
-      <el-button type="primary" @click="create">创建房间</el-button>
-      <el-button type="primary" @click="join">加入房间</el-button>
+  <div id="page">
+    <div class="header"></div>
+    <div class="content clearfix">
+      <div class="left">
+        <div class="title">Game Team Scoring System</div>
+        <div class="button">
+          <div class="button-create" @click="create">Create</div>
+          <div class="button-join">
+            <div class="heading">请输入房间号 <span>RoomId</span></div>
+            <div class="content">
+              <div class="input-group">
+                <input
+                  autocomplete="off"
+                  v-model="roomId"
+                  class="input"
+                  type="text"
+                />
+                <div></div>
+              </div>
+            </div>
+            <button @click="join">Join</button>
+          </div>
+        </div>
+      </div>
+      <div class="right">
+        <div class="image"></div>
+      </div>
     </div>
+    <div class="footer"></div>
   </div>
 </template>
 
 <script lang="ts">
-import {reactive, ref, onMounted, computed, watch, onBeforeUnmount} from 'vue';
-import {useRoute, useRouter} from 'vue-router'
-import {useStore,Commit} from 'vuex';
-import {checkToken} from "@/util/Token";
-import router from "@/router";
-import { ElMessage } from 'element-plus';
+import {
+  reactive,
+  ref,
+  onMounted,
+  computed,
+  watch,
+  onBeforeUnmount,
+} from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { useStore, Commit } from "vuex";
+import { checkToken } from "../../util/Token";
+import router from "../../router";
+import { ElMessage } from "element-plus";
 
-let token: string
+let token: string;
 // 存储token
 if (!checkToken()) {
-  token = new Date().getTime().toString()
-  localStorage.setItem('token', token)
+  token = new Date().getTime().toString();
+  localStorage.setItem("token", token);
 } else {
-  token = localStorage.getItem('token')!
+  token = localStorage.getItem("token")!;
 }
 
 export default {
   setup() {
-    const router = useRouter()
-    const roomId = ref('')
+    const router = useRouter();
+    const roomId = ref("");
     const store = useStore();
     const ws = ref<WebSocket | null>(null);
 
-    const createws =  () => {
-      const wss = store.state.websocket
-      ws.value = wss
+    const createws = () => {
+      const wss = store.state.websocket;
+      ws.value = wss;
       // 监听信息
-      wss.onmessage = (event: { data: string; }) => {
-        const data = JSON.parse(event.data)
-        console.log(data)
-        if (data.type === 'create') {
-          store.commit('SET_SIGN',0)
-          create_router(data.roomId)
+      wss.onmessage = (event: { data: string }) => {
+        const data = JSON.parse(event.data);
+        console.log(data);
+        if (data.type === "create") {
+          store.commit("SET_SIGN", 0);
+          create_router(data.roomId);
         }
-      }
+      };
     };
     // 创建房间
     const create = () => {
-      ws.value?.send(JSON.stringify({type: 'create'}));
+      ws.value?.send(JSON.stringify({ type: "create" }));
     };
 
     // 创建房间跳转
     const create_router = (roomId: string) => {
       router.replace({
-        name: 'roomDetail',
+        name: "roomDetail",
         params: {
           roomId: roomId,
-        }
-      })
+        },
+      });
     };
     // 加入房间
     const join = () => {
-      if (roomId.value === ''){
+      if (roomId.value === "") {
         ElMessage({
-          message: '房间号不能为空',
-          type: 'warning',
-        })
-        return
+          message: "房间号不能为空",
+          type: "warning",
+        });
+        return;
       }
-      store.commit('SET_SIGN',1)
-      create_router(roomId.value)
+      store.commit("SET_SIGN", 1);
+      create_router(roomId.value);
     };
 
     onMounted(() => {
       createws();
     });
-
     return {
       roomId,
       create,
       join,
-      create_router
-    }
-  }
-}
+      create_router,
+    };
+  },
+};
 </script>
 
-<style scoped>
-
+<style lang="less">
+@import url("@/assets/less/base.less");
+@import url("./index.less");
 </style>
